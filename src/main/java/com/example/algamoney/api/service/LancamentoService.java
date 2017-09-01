aad991e667d6497b43f2c2cd049506f8fa733579
asouza <asouza@CCI0167.coffeyconstruction.com>
2017-09-01 15:41:49 +0100

    5.6. Regra para não salvar pessoa inativa

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -9,27 +9,40 @@ import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
 @Service
 public class LancamentoService {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
-	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
-		Lancamento lancamentoSalva = buscarLancamentoPeloCodigo(codigo);
+//	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+//		Lancamento lancamentoSalva = buscarLancamentoPeloCodigo(codigo);
+//
+//		BeanUtils.copyProperties(lancamento, lancamentoSalva, "codigo");
+//		return lancamentoRepository.save(lancamentoSalva);
+//	}
 
-		BeanUtils.copyProperties(lancamento, lancamentoSalva, "codigo");
-		return lancamentoRepository.save(lancamentoSalva);
-	}
+//	private Lancamento buscarLancamentoPeloCodigo(Long codigo) {
+//		Lancamento lancamentoSalva = lancamentoRepository.findOne(codigo);
+//
+//		if (lancamentoSalva == null) {
+//			throw new EmptyResultDataAccessException(1);
+//		}
+//		return lancamentoSalva;
+//	}
 
-	private Lancamento buscarLancamentoPeloCodigo(Long codigo) {
-		Lancamento lancamentoSalva = lancamentoRepository.findOne(codigo);
-
-		if (lancamentoSalva == null) {
-			throw new EmptyResultDataAccessException(1);
+	public Lancamento salvar(Lancamento lancamento) {
+		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
 		}
-		return lancamentoSalva;
+		
+		return lancamentoRepository.save(lancamento);
 	}
 
 }
