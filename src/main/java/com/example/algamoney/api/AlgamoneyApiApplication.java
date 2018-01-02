799cd1e33167bb547dc6822a003f0bcbafaf69b6
alexpt2000gmit <g00317835@gmit.ie>
2018-01-02 15:40:07 +0000

    7.2. Profiles do Spring

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -2,11 +2,14 @@ package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
-	//test GIT
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
 	}
