d542020d8acaf69ac4421bd2e314ed06938b987e
alexpt2000gmit <g00317835@gmit.ie>
2018-01-05 13:56:04 +0000

    11.2. Respondendo a eventos do hospedeiro com @HostListener

--- Exemplos Angular/MEUPROJETO/src/app/campo-colorido.directive.ts
+++ Exemplos Angular/MEUPROJETO/src/app/campo-colorido.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+
 
 
 @Directive({
@@ -9,8 +10,14 @@ export class CampoColoridoDirective {
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
-  ) {
+  ) { }
+
+  @HostListener('focus') aoGanharFocu() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  }
+
+  @HostListener('blur') onPerderFocu() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
