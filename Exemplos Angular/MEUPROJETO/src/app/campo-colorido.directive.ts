1038f43308a9732acf843a87424f673a5d6e34d2
alexpt2000gmit <g00317835@gmit.ie>
2018-01-05 14:34:48 +0000

    11.4. Usando property binding em diretivas customizadas

--- Exemplos Angular/MEUPROJETO/src/app/campo-colorido.directive.ts
+++ Exemplos Angular/MEUPROJETO/src/app/campo-colorido.directive.ts
@@ -1,22 +1,21 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
+  // tslint:disable-next-line:no-input-rename
+  @Input('appCampoColorido') cor = 'gray';
+
   @HostBinding('style.backgroundColor') corDeFundo: string;
 
-
-
   @HostListener('focus') aoGanharFocu() {
-    this.corDeFundo = 'yellow';
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+    this.corDeFundo = this.cor;
   }
 
   @HostListener('blur') onPerderFocu() {
     this.corDeFundo = 'transparent';
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
