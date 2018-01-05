9483a792744d73207aa108d98fcbcc6df7b5b23f
alexpt2000gmit <g00317835@gmit.ie>
2018-01-05 20:48:09 +0000

    12.6. Two-way binding com ngModel

--- Exemplos Angular/Driven-Forms/src/app/app.component.ts
+++ Exemplos Angular/Driven-Forms/src/app/app.component.ts
@@ -1,19 +1,32 @@
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  cliente = new Cliente();
 
   profissoes = ['Programador', 'Empresario', 'Outra'];
   profissao = 'Outra';
 
   salvar(form: NgForm) {
-    console.log(form);
-    console.log(form.value.nome);
+    // this.cliente.nome = form.value.primeiroNome;
+    // this.cliente.email = form.value.emailAddress;
+    // this.cliente.profissao = form.value.profissao;
+
+    console.log(form.value);
+    console.log(this.cliente);
   }
 
 }
