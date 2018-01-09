6f6a5f18bb507e6c81c0fb935cb5652d75c81454
alexpt2000gmit <g00317835@gmit.ie>
2018-01-09 14:54:04 +0000

    13.7. Adicionando campo com máscara (componente InputMask)

--- FrontEnd_Angular/algamoney-ui/src/app/app.module.ts
+++ FrontEnd_Angular/algamoney-ui/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtexta
 import { CalendarModule } from 'primeng/components/calendar/calendar';
 import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
 import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
 
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 
@@ -32,6 +33,7 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
     InputTextModule,
     ButtonModule,
     DataTableModule,
@@ -40,6 +42,8 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
+    InputMaskModule,
+
     CurrencyMaskModule
   ],
   providers: [],
