39ec549e54b04be683c954cbb3c8892afe7c2a50
alexpt2000gmit <g00317835@gmit.ie>
2018-01-14 16:03:39 +0000

    17.11. Alterando o locale da aplicação para pt-BR

--- FrontEnd_Angular/algamoney-ui/src/app/app.module.ts
+++ FrontEnd_Angular/algamoney-ui/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { ToastyModule } from 'ng2-toasty';
 import {ConfirmDialogModule, ConfirmationService} from 'primeng/primeng';
@@ -13,6 +13,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoaService } from './pessoas/pessoa.service';
 import { LancamentoService } from './lancamentos/lancamento.service';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { LancamentoService } from './lancamentos/lancamento.service';
   providers: [
     LancamentoService,
     PessoaService,
-    ConfirmationService
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
